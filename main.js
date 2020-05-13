9c68452ccb01849af3ab3599f717351219f13f63
JHenrysson <56089745+JHenrysson@users.noreply.github.com>
2020-05-13 13:37:56 +0200

    asked for name/move
    used a while loop = for name/loop

--- main.js
+++ main.js
@@ -4,7 +4,17 @@ window.onload = function() {
         menuChoice = prompt('Menu\n1. Create a new game\n2. Exit');
 
         if (menuChoice === '1') {
-            alert("Starting Game")
+            let name = prompt('Enter Name');
+            let gameMove;
+            while (gameMove !== '1'&& gameMove !=='2'&& gameMove !== '3') {
+
+                gameMove = prompt('Enter Move\n1. Rock\n2. Paper\n3. Scissors');
+                if (gameMove === '1' || gameMove === '2' || gameMove === '3') {
+                } else {
+                    alert("Try Again!");
+                }
+            }
+
         } else if (menuChoice === '2') {
             alert('bye');
         } else {
